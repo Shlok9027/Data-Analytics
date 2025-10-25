f0093f7143ba4736cf946709dc92d53cd93f8b0e
SHLOK KUMAR <shlok9027@gmail.com>
2025-10-25 19:00:09 +0530

    add

--- MATPLOTLIB/analysis.py
+++ MATPLOTLIB/analysis.py
@@ -275,15 +275,74 @@
  # # # # # # # # # # # # # # # # PIE CHART # # # # # # # # # # # # # # # # # # # 
 
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# cars = ['BMW', 'AUDI', 'FORD', 'TESLA', 'HONDA','TOYOTA']
+
+# data = [354,322,243,325,265,299]
+
+# fig = plt.figure(figsize=(8,6))
+
+# plt.pie(data, labels=cars, autopct='%1.1f%%', startangle=140)
+
+# plt.show()
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-cars = ['BMW', 'AUDI', 'FORD', 'TESLA', 'HONDA','TOYOTA']
+# Car names
+cars = ['BMW', 'AUDI', 'FORD', 'TESLA', 'HONDA', 'TOYOTA']
 
-data = [354,322,243,325,265,299]
+# Corresponding sales data
+data = [354, 322, 243, 325, 265, 299]
 
-fig = plt.figure(figsize=(8,6))
+# Explode certain slices outward for emphasis
+# Values represent fraction of the radius (e.g., 0.1 = 10%)
+explode = (0.1, 0.0, 0.2, 0.3, 0.0, 0.0)
 
-plt.pie(data, labels=cars, autopct='%1.1f%%', startangle=140)
+# Colors for each slice
+colors = ['red', 'blue', 'beige', 'orange', 'purple', 'yellow']
 
+# Green border around each wedge
+wp = {'linewidth': 1, 'edgecolor': 'green'}
+
+# Function to format percentage + actual value
+def func(pct, allvalues):
+    absolute = int(pct / 100. * np.sum(allvalues))
+    return "{:.1f}%\n({:d} g)".format(pct, absolute)
+
+# Create figure and axis
+fig, ax = plt.subplots(figsize=(10, 7))
+
+# Create pie chart
+wedges, texts, autotexts = ax.pie(
+    data,
+    autopct=lambda pct: func(pct, data),
+    explode=explode,
+    labels=cars,
+    shadow=True,
+    colors=colors,
+    startangle=90,        # start from the top
+    wedgeprops=wp,
+    textprops=dict(color="magenta"),
+    counterclock=True     # pie slices drawn anti-clockwise (default)
+)
+
+# Add legend to the side
+ax.legend(
+    wedges,
+    cars,
+    title="Cars",
+    loc='center left',
+    bbox_to_anchor=(1, 0, 0.5, 1)
+)
+
+# Customize label appearance
+plt.setp(autotexts, size=8, weight="bold")
+
+# Title of the chart
+ax.set_title("Car Sales Distribution")
+
+# Display chart
 plt.show()
