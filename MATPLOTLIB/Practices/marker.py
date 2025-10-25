f9405660edb5c7ca50265c317b47716e98c49e2f
SHLOK KUMAR <shlok9027@gmail.com>
2025-10-25 22:29:35 +0530

    commiting more knowledge

--- MATPLOTLIB/Practices/marker.py
+++ MATPLOTLIB/Practices/marker.py
@@ -4,10 +4,11 @@ import numpy as np
 x = [0,3,6,1,  5,  0]
 y = [0,6,0,3.5,3.5,0]
 
-plt.plot(x, y, marker='o', linestyle='-', color='blue',label='Data Line')
+plt.plot(x, y, marker='o', linestyle='-', color='blue',label='Data Line',)
 
 plt.title('Line Plot with Markers')
 plt.xlabel('X-axis')
 plt.ylabel('Y-axis')
 plt.legend()
+plt.grid(True)
 plt.show()
